f65a0f2a775a3e3b67a5ad48e752da19739db2a6
Radek Podgorny <radek@podgorny.cz>
2015-10-06 11:55:31 +0200

    fixed wrong pointer comparison

--- libuptimed/urec.c
+++ libuptimed/urec.c
@@ -125,8 +125,8 @@ time_t read_uptime(void) {
 
 
 	/* clock_gettime() failed */
-	f=fopen("/proc/uptime", "r");
-	if (f > 0) {
+	f = fopen("/proc/uptime", "r");
+	if (f) {
 		if (fscanf(f, "%lf", &upseconds) > 0) {
 			fclose(f);
 			return((time_t)upseconds);
